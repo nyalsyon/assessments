f29e9c1c9f3c61a54d113d109dab4de392f8a201
nyalsyon <nyalsyon@gmail.com>
2018-02-26 12:27:56 +0545

    Made able to authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
+	include ActionController::HttpAuthentication::Basic::ControllerMethods
+	include ActionController::HttpAuthentication::Token::ControllerMethods
+	before_action :authenticate
+	include ActionController::MimeResponds
 
+	def authenticate
+	  authenticate_or_request_with_http_basic do |source_app, api_key|
+	    client = ClientAccess.find_by_name(source_app)
+	    client && client.token == api_key
+	  end
+	end
 end
